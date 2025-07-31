9f682f9c08872676bc8c920bb34b09db1e1c92e6
thefirsthero <vukosi90@gmail.com>
2025-07-31 23:28:31 +0200

    moved hamburger menu to right of navbar

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -17,58 +17,42 @@ export function AppHeader() {
 
   return (
     <header className="bg-background sticky top-0 z-50 border-b">
-      <div className="w-full ~max-w-7xl mx-auto flex items-center gap-2 h-14 px-4 md:px-8">
+      <div className="w-full ~max-w-7xl mx-auto flex items-center gap-2 h-14 px-4 md:px-8 justify-between">
         <AppLogo />
-        <div className="ml-4 flex-1 flex items-center justify-between">
-          <nav className="hidden md:flex gap-1">
-            {mainMenu.map((item) => (
-              <Button
-                key={item.to}
-                variant="link"
-                onClick={() => scrollTo(item.to)}
-                className="text-foreground/70 hover:text-foreground"
-              >
-                {item.icon && <item.icon className="mr-2 h-4 w-4" />}
-                {item.title}
-              </Button>
-            ))}
-          </nav>
-          <div className="flex items-center gap-1 md:hidden">
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button size="icon" variant="ghost">
-                  <Menu />
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="left">
-                <nav className="flex flex-col gap-1 pt-4">
-                  {mainMenu.map((item) => (
-                    <Button
-                      key={item.to}
-                      variant="link"
-                      onClick={() => scrollTo(item.to)}
-                      className="text-foreground/70 hover:text-foreground justify-start"
-                    >
-                      {item.icon && <item.icon className="mr-2 h-4 w-4" />}
-                      {item.title}
-                    </Button>
-                  ))}
-                </nav>
-              </SheetContent>
-            </Sheet>
-          </div>
-          <nav className="flex gap-1">
-            <a
-              href={appConfig.github.url}
-              title={appConfig.github.title}
-              target="_blank"
-              rel="noreferrer"
-              className={cn("size-8")}
+        <nav className="hidden md:flex gap-1 flex-1 justify-center">
+          {mainMenu.map((item) => (
+            <Button
+              key={item.to}
+              variant="link"
+              onClick={() => scrollTo(item.to)}
+              className="text-foreground/70 hover:text-foreground"
             >
-              <GitHub />
-              <span className="sr-only">GitHub</span>
-            </a>
-          </nav>
+              {item.icon && <item.icon className="mr-2 h-4 w-4" />}{item.title}
+            </Button>
+          ))}
+        </nav>
+        <div className="flex items-center gap-1 md:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button size="icon" variant="ghost">
+                <Menu />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="right">
+              <nav className="flex flex-col gap-1 pt-4">
+                {mainMenu.map((item) => (
+                  <Button
+                    key={item.to}
+                    variant="link"
+                    onClick={() => scrollTo(item.to)}
+                    className="text-foreground/70 hover:text-foreground justify-start"
+                  >
+                    {item.icon && <item.icon className="mr-2 h-4 w-4" />}{item.title}
+                  </Button>
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </header>
